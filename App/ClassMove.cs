b7e9d59a1191aef5b400dba4794570c68a00a75d
Nikita Boyko <nikitabojko57@gmail.com>
2021-12-02 00:23:45 +0200

    comit file

---
+++ App/ClassMove.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App
+{
+    class Move
+    {
+        private void Say()
+        {
+            Console.WriteLine("I will go...");
+        }
+
+        public void Moving()
+        {
+            Say();
+            Console.WriteLine("I move!");
+        }
+    }
+}
---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Move ob = new Move();
+            ob.Moving();
+        }
+    }
+}
